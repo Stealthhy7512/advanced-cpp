e84e47bcd2bdc2381dc04e6fcd94aa126900b6a2
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-09-25 20:02:14 +0300

    test: create tests for pop_back, const initializer list and rvalue initalizer list constructors

--- advanced-cpp/test.cpp
+++ advanced-cpp/test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 
 #include <iostream>
+#include <vector>
 
 #include <catch2/catch_all.hpp>
 #include "vector.hpp"
@@ -28,4 +29,31 @@ TEST_CASE("Vector can be resized properly", "[Vector]") {
   vec.push_back('c');
   CHECK(vec.size() == 3);
   REQUIRE(vec.capacity() == 4);
+}
+
+TEST_CASE("Vector pop_back works", "[Vector]") {
+  Custom::Vector<int> vec;
+  vec.push_back(1);
+  vec.push_back(2);
+  CHECK(vec.back() == 2);
+
+  vec.pop_back();
+  REQUIRE(vec.size() == 1);
+  REQUIRE(vec.pop_back() == 2);
+}
+
+TEST_CASE("Empty vector disallows pop_back", "[Vector]") {
+  Custom::Vector<double> vec;
+  REQUIRE_THROWS(vec.pop_back());
+}
+
+TEST_CASE("Vector initializer list constructor works", "[Vector]") {
+  std::initializer_list<double> ilist{1., 2., 3.};
+  Custom::Vector<double> vec{ ilist };
+  REQUIRE(vec.size() == 3);
+}
+
+TEST_CASE("Vector rvalue initializer list constructor works", "[Vector]") {
+  Custom::Vector<int> vec{ {1, 2, 3} };
+  REQUIRE(vec.size() == 3);
 }
